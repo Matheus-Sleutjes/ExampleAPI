291f5a4c03428c5022a1d712fc9d090b0cfb83fe
matheus sleutjes <matheussleutjes2014@hotmail.com>
2022-07-27 18:09:49 -0400

    ajuste startup

---
+++ ExampleAPI/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SpaServices.AngularCli;
+
+namespace ExampleAPI
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                //if (env.IsDevelopment())
+                //{
+                    spa.UseAngularCliServer(npmScript: "start");
+                //}
+            });
+        }
+    }
+}
